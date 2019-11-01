17e9a38b3c74e9b9d543712f37f42362848f0e9d
WZihao <35311906+ZHOB@users.noreply.github.com>
2019-10-31 19:31:37 -0600

    wrong test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,12 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void myTest(){
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.countCities());
+        cityList.addOne();
+        assertEquals(2, cityList.countCities());
+    }
 }
